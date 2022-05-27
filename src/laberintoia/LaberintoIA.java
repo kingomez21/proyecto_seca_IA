98b2947b9d2537b0b8df52be801fd364fb586c44
kingomez21 <juansebastiangomezmezu@gmail.com>
2022-05-27 18:30:12 -0500

    amplitud listo, faltan algunas cosas como la profundidad etc

--- src/laberintoia/LaberintoIA.java
+++ src/laberintoia/LaberintoIA.java
@@ -21,56 +21,35 @@ public class LaberintoIA {
         matriz = new Matriz();
         matriz.CargarMundo();
         bsq = new BusquedaAmplitud(matriz.getMatriz());
-        
+
     }
-    
-    public void AgenteBuquedaAmplitud(String verificacion, int[][] estado, int cont){
-        if(cont < 28){
-            int fila = matriz.EncontrarJugador(estado)[0];
-            int colum = matriz.EncontrarJugador(estado)[1];
-            String mov = bsq.verificar(fila, colum);
-            
-            int[][] est = bsq.mover(mov, fila, colum);
-            matriz.mostrarMundo(est);
-            System.out.println();
-            cont++;
-            AgenteBuquedaAmplitud(mov, est, cont);
+
+    public void AgenteBuquedaAmplitud(String verificacion, int[][] estado, int cont) {
+        try {
+            if (cont < 29) {
+                int fila = matriz.EncontrarJugador(estado)[0];
+                int colum = matriz.EncontrarJugador(estado)[1];
+                String mov = bsq.verificar(fila, colum);
+                int[][] est = bsq.mover(mov, fila, colum);
+                System.err.println("Nodo: "+bsq.getNodo()+" operador: "+bsq.getOperador()+" profundidad: "+bsq.getProfundidad());
+                matriz.mostrarMundo(est);
+                System.out.println();
+                cont++;
+                AgenteBuquedaAmplitud(mov, est, cont);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.print("El Jugador quedo encerrado");
         }
+
     }
-    
+
     public static void main(String[] args) {
-        
+
         LaberintoIA game = new LaberintoIA();
         String inicial = game.bsq.verificar(2, 2);
-        int [][] m = game.matriz.getMatriz();
-        game.AgenteBuquedaAmplitud(inicial, m , 0);
-        //amplitud.moverDerecha();
-        /*
-        m.mostrarMundo(amplitud.moverDerecha(m.EncontrarJugador(m.getMatriz())[0], m.EncontrarJugador(m.getMatriz())[1]));
-        System.out.println(amplitud.getNodo());
-        System.out.println(amplitud.getOperador());
-        System.out.println();
-        m.mostrarMundo(amplitud.moverDerecha(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
-        System.out.println(amplitud.getNodo());
-        System.out.println(amplitud.getOperador());
-        System.out.println();
-        m.mostrarMundo(amplitud.moverArriba(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
-        System.out.println(amplitud.getNodo());
-        System.out.println(amplitud.getOperador());
-        System.out.println();
-         m.mostrarMundo(amplitud.moverArriba(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
-        System.out.println(amplitud.getNodo());
-        System.out.println(amplitud.getOperador());
-        System.out.println();
-        m.mostrarMundo(amplitud.moverArriba(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
-        System.out.println(amplitud.getNodo());
-        System.out.println(amplitud.getOperador());
-        System.out.println();
-        m.mostrarMundo(amplitud.moverAbajo(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
-        System.out.println(amplitud.getNodo());
-        System.out.println(amplitud.getOperador());
-        System.out.println();*/
-        
+        int[][] m = game.matriz.getMatriz();
+        game.AgenteBuquedaAmplitud(inicial, m, 0);
+
     }
-    
+
 }
