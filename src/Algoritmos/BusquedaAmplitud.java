ee0b8803dc994c7a0ce2b784b9249da8e55e4390
kingomez21 <juansebastiangomezmezu@gmail.com>
2022-05-27 15:47:07 -0500

    amplitud igual al de python, falta mejorar y hacer mas casos

--- src/Algoritmos/BusquedaAmplitud.java
+++ src/Algoritmos/BusquedaAmplitud.java
@@ -17,20 +17,58 @@ public class BusquedaAmplitud {
     String Nodo;
     int profundidad;
     String operador;
-    Matriz m;
     
     public BusquedaAmplitud(int[][] estado){
-        //m = new Matriz();
-        //m.CargarMundo();
         this.Estado = estado;
     }
     
     
     
+    public int[][] moverIzquierda(int fila, int colum){
+        int movColum = colum - 1;
+        
+        if(movColum < 0){
+            movColum = 0;
+        }else 
+            if(movColum > 9){
+            movColum = 9;
+        }
+        
+        this.Estado[fila][colum] = 7;
+        this.Estado[fila] [movColum] = 2;
+        this.operador = "izquierda";
+        this.Nodo = "("+fila+","+movColum+ ")";
+        return this.Estado;
+    }
+    
+    public int[][] moverArriba(int fila, int colum){
+        int movFila = fila - 1;
+        
+        if(movFila < 0){
+            movFila = 0;
+        }else 
+            if(movFila > 9){
+            movFila = 9;
+        }
+        
+        this.Estado[fila][colum] = 7;
+        this.Estado[movFila][colum] = 2;
+        this.operador = "arriba";
+        this.Nodo = "("+movFila+","+colum+ ")";
+        return this.Estado;
+    }
+    
     public int[][] moverDerecha(int fila, int colum){
         int movColum = colum + 1;
         
-        this.Estado[fila][colum] = 10;
+        if(movColum < 0){
+            movColum = 0;
+        }else 
+            if(movColum > 9){
+            movColum = 9;
+        }
+        
+        this.Estado[fila][colum] = 7;
         this.Estado[fila] [movColum] = 2;
         this.operador = "derecha";
         this.Nodo = "("+fila+","+movColum+ ")";
@@ -38,6 +76,129 @@ public class BusquedaAmplitud {
         return this.Estado;
     }
     
+    public int[][] moverAbajo(int fila, int colum){
+        int movFila = fila + 1;
+        
+        if(movFila < 0){
+            movFila = 0;
+        }else 
+            if(movFila > 9){
+            movFila = 9;
+        }
+        
+        this.Estado[fila][colum] = 7;
+        this.Estado[movFila][colum] = 2;
+        this.operador = "abajo";
+        this.Nodo = "("+movFila+","+colum+ ")";
+        return this.Estado;
+    }
+    
+    public String verificar(int fila, int colum){
+        
+        String movimientos = "";
+        
+        int movColumIz = colum - 1;
+        
+        if(movColumIz >= 0){
+            int izquierda = this.Estado[fila][movColumIz];
+            if(izquierda != 1 && izquierda != 7){
+                movimientos += "1";
+            }else {
+                movimientos += "0";
+            }
+            
+        }else {
+            movimientos += "0";
+        }
+        
+        int movFilaAr = fila - 1;
+        
+        if(movFilaAr >= 0){
+            int arriba = this.Estado[movFilaAr][colum];
+            if(arriba != 1 && arriba != 7){
+                movimientos += "1";
+            }else {
+                movimientos += "0";
+            }
+        }else {
+            movimientos += "0";
+        }
+        
+        int movColumDer = colum + 1;
+        
+        if(movColumDer >= 0){
+            int derecha = this.Estado[fila][movColumDer];
+            if(derecha != 1 && derecha != 7){
+                movimientos += "1";
+            }else {
+                movimientos += "0";
+            }
+        }else {
+            movimientos += "0";
+        }
+        
+        int movFilaAba = fila + 1;
+        
+        if(movFilaAba >= 0){
+            if(movFilaAba > 9){
+                movFilaAba = fila;
+            }
+            
+            int abajo = this.Estado[movFilaAba][colum];
+            if(abajo != 1 && abajo != 7){
+                movimientos += "1";
+            }else {
+                movimientos += "0";
+            }
+        }else {
+            movimientos += "0";
+        }
+        
+        
+        return movimientos; 
+    }
+    
+    
+    public int[][] mover(String movimientos, int fila, int colum){
+        
+        if(movimientos.equalsIgnoreCase("1010")){
+            return this.moverDerecha(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("1001")){
+            return this.moverIzquierda(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("0100")){
+            return this.moverArriba(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("0010")){
+            return this.moverDerecha(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("0110")){
+            return this.moverArriba(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("1000")){
+            return this.moverIzquierda(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("0001")){
+            return this.moverAbajo(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("0011")){
+            return this.moverDerecha(fila, colum);
+        }
+        
+        if(movimientos.equalsIgnoreCase("0111")){
+            return this.moverDerecha(fila, colum);
+        }
+        
+        return this.Estado;
+    }
     
     public int[][] getEstado(){
         return this.Estado;
--- src/laberintoia/LaberintoIA.java
+++ src/laberintoia/LaberintoIA.java
@@ -14,24 +14,63 @@ import Model.Matriz;
  */
 public class LaberintoIA {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Matriz m = new Matriz();
-        m.CargarMundo();
-        //System.out.println(m.EncontrarJugador(m.getMatriz())[0]);
-        //System.out.println(m.EncontrarJugador(m.getMatriz())[1]);
-        BusquedaAmplitud amplitud = new BusquedaAmplitud(m.getMatriz());
+    Matriz matriz;
+    BusquedaAmplitud bsq;
 
+    public LaberintoIA() {
+        matriz = new Matriz();
+        matriz.CargarMundo();
+        bsq = new BusquedaAmplitud(matriz.getMatriz());
+        
+    }
+    
+    public void AgenteBuquedaAmplitud(String verificacion, int[][] estado, int cont){
+        if(cont < 28){
+            int fila = matriz.EncontrarJugador(estado)[0];
+            int colum = matriz.EncontrarJugador(estado)[1];
+            String mov = bsq.verificar(fila, colum);
+            
+            int[][] est = bsq.mover(mov, fila, colum);
+            matriz.mostrarMundo(est);
+            System.out.println();
+            cont++;
+            AgenteBuquedaAmplitud(mov, est, cont);
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        LaberintoIA game = new LaberintoIA();
+        String inicial = game.bsq.verificar(2, 2);
+        int [][] m = game.matriz.getMatriz();
+        game.AgenteBuquedaAmplitud(inicial, m , 0);
         //amplitud.moverDerecha();
+        /*
         m.mostrarMundo(amplitud.moverDerecha(m.EncontrarJugador(m.getMatriz())[0], m.EncontrarJugador(m.getMatriz())[1]));
         System.out.println(amplitud.getNodo());
         System.out.println(amplitud.getOperador());
         System.out.println();
-        System.out.println(m.EncontrarJugador(amplitud.getEstado())[0]);
-        System.out.println(m.EncontrarJugador(amplitud.getEstado())[1]);
         m.mostrarMundo(amplitud.moverDerecha(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
+        System.out.println(amplitud.getNodo());
+        System.out.println(amplitud.getOperador());
+        System.out.println();
+        m.mostrarMundo(amplitud.moverArriba(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
+        System.out.println(amplitud.getNodo());
+        System.out.println(amplitud.getOperador());
+        System.out.println();
+         m.mostrarMundo(amplitud.moverArriba(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
+        System.out.println(amplitud.getNodo());
+        System.out.println(amplitud.getOperador());
+        System.out.println();
+        m.mostrarMundo(amplitud.moverArriba(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
+        System.out.println(amplitud.getNodo());
+        System.out.println(amplitud.getOperador());
+        System.out.println();
+        m.mostrarMundo(amplitud.moverAbajo(m.EncontrarJugador(amplitud.getEstado())[0], m.EncontrarJugador(amplitud.getEstado())[1]));
+        System.out.println(amplitud.getNodo());
+        System.out.println(amplitud.getOperador());
+        System.out.println();*/
+        
     }
     
 }
