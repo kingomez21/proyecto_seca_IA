5b98e35d496920414f1a0508da41fc3a43cff91a
kingomez21 <juansebastiangomezmezu@gmail.com>
2022-06-01 00:58:15 -0500

    Costo uniforme implementado

---
+++ src/Algoritmos/BusquedaCosto.java
@@ -0,0 +1,231 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algoritmos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class BusquedaCosto {
+
+    int[][] Estado = new int[10][10];
+    String Nodo;
+    int profundidad;
+    int Costo;
+    String operador;
+
+    public BusquedaCosto(int[][] estado) {
+        this.Estado = estado;
+    }
+
+    public int[][] moverIzquierda(int fila, int colum) {
+        int movColum = colum - 1;
+
+        if (movColum < 0) {
+            movColum = 0;
+        } else if (movColum > 9) {
+            movColum = 9;
+        }
+
+        this.Estado[fila][colum] = 7;
+        this.Estado[fila][movColum] = 2;
+        this.operador = "izquierda";
+        this.Nodo = "(" + fila + "," + movColum + ")";
+        this.profundidad += 1;
+        return this.Estado;
+    }
+
+    public int[][] moverArriba(int fila, int colum) {
+        int movFila = fila - 1;
+
+        if (movFila < 0) {
+            movFila = 0;
+        } else if (movFila > 9) {
+            movFila = 9;
+        }
+
+        this.Estado[fila][colum] = 7;
+        this.Estado[movFila][colum] = 2;
+        this.operador = "arriba";
+        this.Nodo = "(" + movFila + "," + colum + ")";
+        this.profundidad += 1;
+        return this.Estado;
+    }
+
+    public int[][] moverDerecha(int fila, int colum) {
+        int movColum = colum + 1;
+
+        if (movColum < 0) {
+            movColum = 0;
+        } else if (movColum > 9) {
+            movColum = 9;
+        }
+
+        this.Estado[fila][colum] = 7;
+        this.Estado[fila][movColum] = 2;
+        this.operador = "derecha";
+        this.Nodo = "(" + fila + "," + movColum + ")";
+        this.profundidad += 1;
+        return this.Estado;
+    }
+
+    public int[][] moverAbajo(int fila, int colum) {
+        int movFila = fila + 1;
+
+        if (movFila < 0) {
+            movFila = 0;
+        } else if (movFila > 9) {
+            movFila = 9;
+        }
+
+        this.Estado[fila][colum] = 7;
+        this.Estado[movFila][colum] = 2;
+        this.operador = "abajo";
+        this.Nodo = "(" + movFila + "," + colum + ")";
+        this.profundidad += 1;
+        return this.Estado;
+    }
+
+    public String verficarCostoCaminos(int fila, int colum) {
+
+        String mover = "";
+        int iz = 1000, arr = 1000, der = 1000, aba = 1000;
+
+        int movColumIz = colum - 1;
+
+        if (movColumIz >= 0) {
+            if (movColumIz > 9) {
+                movColumIz = colum;
+            }
+            int izquierda = this.Estado[fila][movColumIz];
+
+            if (izquierda != 1 && izquierda != 7 && izquierda != 2) {
+                iz = this.Costo + izquierda;
+            } else {
+                iz = 1000;
+            }
+
+        }
+
+        int movFilaAr = fila - 1;
+
+        if (movFilaAr >= 0) {
+            if (movFilaAr > 9) {
+                movFilaAr = fila;
+            }
+            int arriba = this.Estado[movFilaAr][colum];
+
+            if (arriba != 1 && arriba != 7 && arriba != 2) {
+                arr = this.Costo + arriba;
+            } else {
+                arr = 1000;
+            }
+
+        }
+
+        int movColumDer = colum + 1;
+
+        if (movColumDer >= 0) {
+            if (movColumDer > 9) {
+                movColumDer = colum;
+            }
+
+            int derecha = this.Estado[fila][movColumDer];
+
+            if (derecha != 1 && derecha != 7 && derecha != 2) {
+                der = this.Costo + derecha;
+            } else {
+                der = 1000;
+            }
+
+        }
+
+        int movFilaAba = fila + 1;
+
+        if (movFilaAba >= 0) {
+            if (movFilaAba > 9) {
+                movFilaAba = fila;
+            }
+
+            int abajo = this.Estado[movFilaAba][colum];
+
+            if (abajo != 1 && abajo != 7 && abajo != 2) {
+                aba = this.Costo + abajo;
+            } else {
+                aba = 1000;
+            }
+
+        }
+
+        if (iz != 1000) {
+            if (iz <= arr && iz <= der && iz <= aba) {
+                mover = "IZ";
+            }
+        }
+        if (arr != 1000) {
+            if (arr <= iz && arr <= der && arr <= aba) {
+                mover = "ARR";
+            }
+        }
+        if (der != 1000) {
+            if (der <= arr && der <= iz && der <= aba) {
+                mover = "DER";
+            }
+        }
+        if (aba != 1000) {
+            if (aba <= arr && aba <= iz && aba <= der) {
+                mover = "ABA";
+            }
+        }
+
+        return mover;
+    }
+
+    public int[][] mover(String mov, int fila, int colum) {
+
+        if (mov.equalsIgnoreCase("IZ")) {
+            return this.moverIzquierda(fila, colum);
+        }
+        if (mov.equalsIgnoreCase("ARR")) {
+            return this.moverArriba(fila, colum);
+        }
+        if (mov.equalsIgnoreCase("DER")) {
+            return this.moverDerecha(fila, colum);
+        }
+        if (mov.equalsIgnoreCase("ABA")) {
+            return this.moverAbajo(fila, colum);
+        }
+
+        return this.Estado;
+    }
+
+    public void setCostoAcumulado(int Costo) {
+        this.Costo += Costo;
+    }
+
+    public int[][] getEstado() {
+        return Estado;
+    }
+
+    public String getNodo() {
+        return Nodo;
+    }
+
+    public int getProfundidad() {
+        return profundidad;
+    }
+
+    public int getCosto() {
+        return Costo;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    
+    
+}
--- src/laberintoia/LaberintoIA.java
+++ src/laberintoia/LaberintoIA.java
@@ -6,6 +6,7 @@
 package laberintoia;
 
 import Algoritmos.BusquedaAmplitud;
+import Algoritmos.BusquedaCosto;
 import Model.Matriz;
 
 /**
@@ -16,11 +17,14 @@ public class LaberintoIA {
 
     Matriz matriz;
     BusquedaAmplitud bsq;
+    BusquedaCosto bsqC;
 
     public LaberintoIA() {
+
         matriz = new Matriz();
         matriz.CargarMundo();
         bsq = new BusquedaAmplitud(matriz.getMatriz());
+        bsqC = new BusquedaCosto(matriz.getMatriz());
 
     }
 
@@ -31,24 +35,47 @@ public class LaberintoIA {
                 int colum = matriz.EncontrarJugador(estado)[1];
                 String mov = bsq.verificar(fila, colum);
                 int[][] est = bsq.mover(mov, fila, colum);
-                System.err.println("Nodo: "+bsq.getNodo()+" operador: "+bsq.getOperador()+" profundidad: "+bsq.getProfundidad());
+                System.out.println("Nodo: " + bsq.getNodo() + " operador: " + bsq.getOperador() + " profundidad: " + bsq.getProfundidad());
                 matriz.mostrarMundo(est);
                 System.out.println();
                 cont++;
                 AgenteBuquedaAmplitud(mov, est, cont);
             }
         } catch (IndexOutOfBoundsException e) {
-            System.out.print("El Jugador quedo encerrado");
+            System.err.print("El Jugador quedo encerrado");
         }
 
+    }
+
+    public void AgenteBusquedaCostoUniforme(int[][] estado, int cont) {
+        try {
+            if (cont < 29) {
+                int fila = matriz.EncontrarJugador(estado)[0];
+                int colum = matriz.EncontrarJugador(estado)[1];
+                String verficacion = bsqC.verficarCostoCaminos(fila, colum);
+                bsqC.setCostoAcumulado(bsqC.getProfundidad());
+                int[][] est = bsqC.mover(verficacion, fila, colum);
+                matriz.mostrarMundo(est);
+                System.out.println();
+                cont++;
+                AgenteBusquedaCostoUniforme(est, cont);
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Jugador quedo encerrado");
+        }
     }
 
     public static void main(String[] args) {
 
         LaberintoIA game = new LaberintoIA();
-        String inicial = game.bsq.verificar(2, 2);
         int[][] m = game.matriz.getMatriz();
-        game.AgenteBuquedaAmplitud(inicial, m, 0);
+        int fila = game.matriz.EncontrarJugador(m)[0];
+        int colum = game.matriz.EncontrarJugador(m)[1];
+        String inicial = game.bsq.verificar(fila, colum);
+        //game.AgenteBuquedaAmplitud(inicial, m, 0);
+
+        game.AgenteBusquedaCostoUniforme(m, 0);
 
     }
 
